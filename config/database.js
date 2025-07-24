6e3c85d6fac53b3f8c75117e4180e55387c0a38d
Marcin Doliszny <marcin.doliszny@macbookpro.home>
2025-07-24 15:00:03 +0200

    Recreate config files with fresh syntax

--- config/database.js
+++ config/database.js
@@ -1,19 +1,9 @@
-# In your creed-tests folder, replace the database config with a simpler version:
-cat > config/database.js << 'EOF'
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: "postgres",
     connection: {
-      connectionString: env('DATABASE_URL'),
-      ssl: {
-        rejectUnauthorized: false
-      }
-    },
-    acquireConnectionTimeout: 60000,
-    pool: {
-      min: 2,
-      max: 10
+      connectionString: env("DATABASE_URL"),
+      ssl: { rejectUnauthorized: false }
     }
   }
 });
-EOF
--- config/server.js
+++ config/server.js
@@ -1,30 +1,7 @@
-# Check server.js
-cat > config/server.js << 'EOF'
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   app: {
-    keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  },
+    keys: env.array("APP_KEYS")
+  }
 });
-EOF
-
-# Check admin.js
-cat > config/admin.js << 'EOF'
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
-  apiToken: {
-    salt: env('API_TOKEN_SALT'),
-  },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-});
-EOF
