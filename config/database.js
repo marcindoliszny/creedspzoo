7e62e74f90d999d76349b8003722d7b787147434
Marcin Doliszny <marcin.doliszny@macbookpro.home>
2025-07-24 14:56:58 +0200

    Fix JavaScript syntax in config files

--- config/database.js
+++ config/database.js
@@ -1,37 +1,19 @@
-# In your creed-tests folder, update the database config:
+# In your creed-tests folder, replace the database config with a simpler version:
 cat > config/database.js << 'EOF'
-const path = require("path");
-
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT", "sqlite");
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        ssl: env.bool("DATABASE_SSL", true) && {
-          rejectUnauthorized: env.bool("DATABASE_SSL_REJECT_UNAUTHORIZED", false),
-        },
-      },
-      pool: { 
-        min: env.int("DATABASE_POOL_MIN", 2), 
-        max: env.int("DATABASE_POOL_MAX", 10) 
-      },
-    },
-    sqlite: {
-      connection: {
-        filename: path.join(__dirname, "..", "..", env("DATABASE_FILENAME", "data.db")),
-      },
-      useNullAsDefault: true,
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'postgres',
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      connectionString: env('DATABASE_URL'),
+      ssl: {
+        rejectUnauthorized: false
+      }
     },
-  };
-};
+    acquireConnectionTimeout: 60000,
+    pool: {
+      min: 2,
+      max: 10
+    }
+  }
+});
 EOF
